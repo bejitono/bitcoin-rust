dfb0e3f9699cefc399665e99d98d6c6d806406f1
Stefano <48912582+bejitono@users.noreply.github.com>
2024-10-18 13:58:39 +0700

    Adds constants.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,6 +1,21 @@
 use serde::{Deserialize, Serialize};
 use uint::construct_uint;
 
+// Initial reward in bitcoins
+pub const INITIAL_REWARD: u64 = 50;
+// Halving interval in blocks
+pub const HAVLING_INTERVAL: u64 = 210;
+// Ideal block time in seconds
+pub const IDEAL_BLOCK_TIME: u64 = 10;
+pub const MIN_TARGET: U256 = U256([
+    0xFFFF_FFFF_FFFF_FFFF,
+    0xFFFF_FFFF_FFFF_FFFF,
+    0xFFFF_FFFF_FFFF_FFFF,
+    0x0000_FFFF_FFFF_FFFF,
+]);
+// Difficulty update interval in blocks
+pub const DIFFICULTY_UPDATE_INTERVAL: u64 = 50;
+
 construct_uint! {
     // Construct an unsigned 256-bit integer
     // consisting of 4 * 64-bit words
